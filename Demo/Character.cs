6e3a0d1c0bfd25d4d4674cb83a5c743267847cc8
Lucas Ying <lucas.yingmm@gmail.com>
2017-05-16 18:47:41 +1200

    built a simple character class with simple logics

---
+++ Demo/Character.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Character
+    {
+        public int Health { get; private set; } = 100;
+        public bool IsDead { get; private set; }
+        public int DamageResistance { get; set; }
+        public string Race { get; set; }
+
+        public void Hit(int damage)
+        {
+            var raceResistance = 0;
+
+            if (Race == "Elf")
+            {
+                raceResistance = 20;
+            }
+
+            var totalDamageTaken = Math.Max(damage - raceResistance - DamageResistance, 0);
+
+            Health -= totalDamageTaken;
+
+            if (Health <= 0)
+            {
+                IsDead = true;
+            }
+        }
+    }
+}
